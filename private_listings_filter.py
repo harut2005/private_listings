c662aab55a28072323ecf473ba5ff3116787dad4
harut2005 <harut2005@gmail.com>
2019-04-13 12:33:19 +0200

    fixed xe.gr new page

--- private_listings_filter.py
+++ private_listings_filter.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
-import urllib, urllib2, re, os, time, io
+import urllib, urllib2, re, os, time, sys
 from datetime import datetime
 
 # general configs
-price = input("Set a price (Euro):\n")
-# price = 78000 # 3
+if (len(sys.argv) > 1):
+	price = sys.argv[1]
+else:
+	price = input("Set a price (Euro):\n")
 
+# price = 75000
 
+print "Price set to: " + str(price) + " €"
 
 time_start = time.time()
 spitogatos_website_timeout = 280
@@ -17,7 +21,7 @@ spitogatos_url += "/uploaded_month"
 spitogatos_url += "/price_nd-"+str(price)
 spitogatos_url += "/offset_0"#+str(offset)
 
-xe_url = "https://www.xe.gr/property/search?Geo.area_id_new__hierarchy=82196&System.item_type=re_residence&Transaction.price.to="+str(price)+"&Transaction.type_channel=117518&per_page=50&sort_by=Publication.effective_date_start&sort_direction=desc&page=2&Publication.age=30"
+xe_url = "https://www.xe.gr/property/search?Geo.area_id_new__hierarchy=82196&System.item_type=re_residence&Transaction.price.to="+str(price)+"&Transaction.type_channel=117518&per_page=50&sort_by=Publication.effective_date_start&sort_direction=desc&page=1&Publication.age=30"
 
 
 
@@ -34,33 +38,16 @@ spitogatos_listings_html_body = ""
 search_timeout = int(time.time())
 count = 0
 
-print "\nSpitogatos started.\n"
+print "\nSearching in Spitogatos.gr.\n"
 while(spitogatos_url):
 	if checkTimeout(search_timeout, spitogatos_website_timeout):
 		search_timeout = time.time()
-	
-	# print spitogatos_url.rsplit("offset_")[0]
+
+
 	print "Searching in page "+str(1+int(spitogatos_url.rsplit("offset_")[1])/10)
 	response = urllib2.urlopen(spitogatos_url)
 	url = response.read()
 	response.close()
-
-
-
-	# match = re.compile(r'media.{1,50}.gr/([^"]+").{1,2800}sg\-icon sg\-icon\-private', re.DOTALL).findall(url)
-	# if match:
-	# 	for link in match:
-	# 		count += 1
-	# 		listing_link = link
-	# 		# print date+price+desc+filt
-	# 		print listing_link
-	# 		# spitogatos_links_list.append(listing_link)
-	# 		spitogatos_listings_html_body += "<a target=\"_blank\" href="+listing_link+">"+listing_link+"</a><br>\n"
-
-
-	# match = re.compile(r'h4 class="text color cyan larger  semibold searchListing_title"><a href="([^"]+)".+?</h4>[^<]+<div[^>]+>([^<]+)</div>[^<]*<div[^>]+>[^<]+<strong[^>]+>(\d{1,2}\/\d{1,2}\/\d{4})[^>]+>([^<]+)<.+?class="listingPrice">[^>]+>([^<]+)\D+(\d+)', re.DOTALL).findall(url))
-	# match = re.compile(r'searchListing_title"><a href="([^"]+)">[^<]+<[^<]+</span></a></h4>[^<]+<div[^>]+>([^<]+)</div>[^<]*<div[^>]+>[^<]+<strong[^>]+>(\d{1,2}\/\d{1,2}\/\d{4})[^>]+>([^<]+)</div>\s*</div>\s*</div>.+?<div class="listingPrice">[^>]+>([^<]+)\D+(\d+)(.{1,800}private)', re.DOTALL).findall(url) #listingIcons)
-	
 
 	all_listings_match = re.compile(r'searchListing_title"><a href="([^"]+".{1,2800})sg\-icon\-private', re.DOTALL).findall(url)
 	if all_listings_match:
@@ -73,7 +60,7 @@ while(spitogatos_url):
 					# print date+price+desc+filt
 					print listing_link
 					# spitogatos_links_list.append(listing_link)
-					spitogatos_listings_html_body += "<a target=\"_blank\" href="+listing_link+"><b>"+price+"</b>  "+size+"m2  ("+date+")  "+region+"  "+desc+"</a><br>\n"	
+					spitogatos_listings_html_body += "<a target=\"_blank\" href="+listing_link+"><b>"+price+"</b>  "+size+"m2  ("+date+")  "+region+"  "+desc+"</a><br>\n"
 
 	next_page = re.findall(r'<link rel="next" href="([^"]+)"',url)
 	# print next_page
@@ -86,7 +73,6 @@ while(spitogatos_url):
 print "\nSpitogatos completed. "+str(count)+" listings found.\n"
 
 
-
 # xe.gr
 
 xe_website_timeout = 1800
@@ -95,14 +81,17 @@ xe_links_list = []
 xe_listings_html_body = ""
 count = 0
 
-print "\nxe.gr started.\n"
+print "\nSearching in Xe.gr.\n"
 while(xe_url):
 	if checkTimeout(search_timeout, xe_website_timeout):
 		search_timeout = time.time()
 
-	page_match = re.search(r"&page=(\d+)&Pub", xe_url)
+	page_match = re.search(r'[^_]page=(\d+)', xe_url)
 	if page_match:
 		print "Searching in page " + str(page_match.group(1))
+	else:
+		print "Last page\n"
+		break
 	# req = urllib2.Request(xe_url)
 	# response = urllib2.urlopen(req)
 	response = urllib2.urlopen(xe_url)
@@ -114,20 +103,18 @@ while(xe_url):
 	if match:
 		for link, desc, price, date in match:
 			count += 1
-			xe_listings_html_body += "<a target=\"_blank\" href="+link+"><b>"+price +"</b>  ("+date+")  "+ desc+"</a><br>\n"
+			xe_listings_html_body += "<a target=\"_blank\" href="+link+"><b>"+ str(price) +"</b>  ("+date+")  "+ desc+"</a><br>\n"
 			print link
 
 
 	next_page = re.findall(r'<td>[^<]+<a href="([^"]+)" class="white_button right+',url)
-	# print next_page
 	if not next_page:
 		xe_url = ""
 		break
 	else:
-		xe_url = next_page[0]
-	# print xe_url
+		xe_url = "https://www.xe.gr"+next_page[0]
 
-	print "\nXE.gr completed. "+str(count)+" listings found.\n"
+print "\nXE.gr completed. "+str(count)+" listings found.\n"
 
 
 
@@ -136,7 +123,7 @@ while(xe_url):
 # write links to an html file
 #<CENTER><IMG SRC="clouds.jpg" ALIGN="BOTTOM"> </CENTER>
 raw_file = '<HTML>\n<HEAD>\n<meta charset="utf-8">\n<TITLE>Spitogatos</TITLE></HEAD>\n<BODY BGCOLOR="FFFFFF">\n'
-raw_file += '<H2>Λίστα με ακίνητα <b>μόνο από ιδιώτες</b> και τιμή μέχρι '+price+' + ' (' + str(datetime.today().strftime("%Y-%m-%d")) + ')</H1>\n'
+raw_file += '<H2>Λίστα με ακίνητα <b>μόνο από ιδιώτες</b> και τιμή μέχρι '+str(price)+' (' + str(datetime.today().strftime("%Y-%m-%d")) + ')</H1>\n'
 # raw_file += '<input type="button" id=\'script\' name="scriptbutton" value=" Ανανέωση " onclick="exec(\'python spitogatos.py\');window.location.reload();">\n'
 raw_file += '<div><a href="https://en.spitogatos.gr/"><img src="https://cdn.spitogatos.gr/frontend/images/logo/logo.header.new.en.png"></a></div>\n'
 raw_file += spitogatos_listings_html_body
@@ -154,4 +141,3 @@ f.write(raw_file)
 f.close
 
 print "\nProgram completed sucessfully in " + '{:.0f}'.format(time.time() - time_start) +" seconds\n"
-
